6b48330aa222043d97b9e328a33f0b4f836b1e12
shinequasar <shinequasar@gmail.com>
2022-10-31 17:39:00 +0900

    [feature/SAN-25]: [REFACTOR] index.js에서 head를 Seo.js로 분리

---
+++ component/common/Seo.js
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+
+export default function Seo({ title }) {
+    return (
+        <Head>
+            <title>{title} | 두근두근 어드벤트 캘린더</title>
+            <meta name="description" content="두근두근 어드벤트 캘린더" />
+            <link rel="icon" href="/asset/image/favicon.ico" />
+        </Head>
+    );
+}
