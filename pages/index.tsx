e4ce1f3c892828868cdb7daf83ca589289ff5cfb
shinequasar <shinequasar@gmail.com>
2022-11-02 11:07:23 +0900

    [feature/SAN-23]: [ADD] styled-components 의존성 추가 및 확인

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,17 @@
 import styles from '../styles/Home.module.css'
 import Seo from "../component/common/Seo";
+import styled from "styled-components";
+
+//styled components 작동되나 확인용 임시코드
+//추후 index.js에서 제거할 예정
+const Button = styled.button`
+  width: 300px;
+  height: 50px;
+  background-color: #0070f3;
+  color: white;
+  font-size: 20px;
+`;
+
 
 export default function Home() {
   return (
@@ -12,6 +24,7 @@ export default function Home() {
             <p className={styles.description}>
               지금부터 프론트엔드 개복수술 들어갑니다
             </p>
+            <Button>임시버튼</Button>
           </main>
     </div>
   )
