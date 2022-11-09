0cf016dc38ab85e99dfb3f17b10ea2c8ede32040
Suyeon-B <tndus4243@naver.com>
2022-11-09 20:39:30 +0900

    [feature/SAN-51][ADD] 화면 크기 하드코딩

--- component/layout/Layout.tsx
+++ component/layout/Layout.tsx
@@ -2,6 +2,8 @@ import Header from "./Header";
 import styled from "styled-components";
 
 const MainWrapper = styled.div`
+    // TODO : 반응형 작업
+    width: 360px;
     background-color: #181c23;
     padding: 22px;
 `;
