222166ab002c9a344d89aebd2ad5cd6893109a96
shinequasar <shinequasar@gmail.com>
2022-11-24 00:43:04 +0900

    [feature/SAN-132]:  [refactor] hook refactor

--- api/hooks/useGetFriend.ts
+++ api/hooks/useGetFriend.ts
@@ -1,8 +1,7 @@
 import FriendsService from "../FriendsService";
-import {AxiosError} from "axios";
 
 export async function useGetFriend() {
         const res = await FriendsService.getFriends();
-        console.log(res.data);
+        // console.log(res.data);
         return res.data;
 }
