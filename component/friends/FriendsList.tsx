bb1d25a2b1c5dbc0ba9df757e504fbc6035f3fe4
Suyeon-B <tndus4243@naver.com>
2022-11-25 22:19:38 +0900

    update : 친구 목록 컴포넌트 고도화

--- component/friends/FriendsList.tsx
+++ component/friends/FriendsList.tsx
@@ -3,11 +3,29 @@ import { setGetFriend } from "../../api/hooks/useGetFriend";
 import Image from "next/image";
 import { Flex } from "../../styles/styledComponentModule";
 import { useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
 
-const FriendsListWrapper = styled.div``;
+const AlignedFlex = styled(Flex)`
+  align-items: center;
+`;
+
+const KakaoShare = styled.div`
+  background-image: url(https://developers.kakao.com/assets/img/about/logos/kakaotalksharing/kakaotalk_sharing_btn_medium.png);
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: contain;
+  width: 2.5rem;
+  cursor: pointer;
+`;
+
+const GoFriendsCalendarBtn = styled(Button)`
+  background-color: #8d362d;
+  border-color: #8d362d;
+`;
 
 const FriendCard = styled.div`
-  width: 35rem;
+  display: flex;
+  align-items: center;
   height: 72px;
   font-size: 30px;
   font-weight: bold;
@@ -16,17 +34,20 @@ const FriendCard = styled.div`
   border-radius: 12px;
   z-index: 5;
   color: white;
+  padding: 1rem;
+  justify-content: space-between;
   @media (max-width: 600px) {
     width: 100%;
-    margin-bottom: 10px;
     height: 62px;
     font-size: 24px;
   }
 `;
 
 const FriendsList = () => {
-  // TODO : friends 데이터 가져와서 작업
   const [friendsData, setFriendsData] = useState<any>();
+  const [currFriendIsFavorite, setCurrFriendIsFavorite] =
+    useState<boolean>(false);
+  // 서버 살아 있다면 ---------------------------------------
   const getFriendsData = async () => {
     const res = await setGetFriend();
     setFriendsData(res);
@@ -43,28 +64,58 @@ const FriendsList = () => {
     console.log("친구 캘린더로 가즈아~");
   };
 
+  const handleClickFavoriteFriend = (isFavorite) => {
+    // 친구한테 데이터 보내야 함
+    console.log("조아용");
+    setCurrFriendIsFavorite(!isFavorite);
+  };
+
   const RenderFriendCardContents = (props) => {
-    console.log(props.profileImgUrl);
     return (
-      <Flex>
-        <Image
-          // src={props.profileImgUrl}
-          src={`/assets/image/character/face_smile.png`}
-          alt="profile-img"
-          width={50}
-          height={50}
-        />
-        <p>{props.name}</p>
+      <>
+        <AlignedFlex>
+          <Image
+            // src={props.profileImgUrl}
+            src={`/assets/image/character/face_smile.png`}
+            alt="profile-img"
+            width={50}
+            height={50}
+          />
+          <div>{props.name}</div>
+          {/* MVP2 : 즐겨찾기 친구 */}
+          {/* {props.isFavorite ? (
+            <Image
+              // src={props.profileImgUrl}
+              src={`/assets/image/friend/fullheart.png`}
+              alt="profile-img"
+              width={30}
+              height={30}
+              onClick={handleClickFavoriteFriend}
+            />
+          ) : (
+            <Image
+              // src={props.profileImgUrl}
+              src={`/assets/image/friend/emptyheart.png`}
+              alt="profile-img"
+              width={30}
+              height={30}
+              onClick={handleClickFavoriteFriend}
+            />
+          )} */}
+        </AlignedFlex>
+
         <Flex>
-          <button onClick={handleClickKakaoShare}>카카오톡 공유</button>
-          <button onClick={goFriendsCalendar}>친구캘린더로</button>
+          <KakaoShare onClick={handleClickKakaoShare}></KakaoShare>
+          <GoFriendsCalendarBtn onClick={goFriendsCalendar}>
+            쪽지 보내러 가기
+          </GoFriendsCalendarBtn>
         </Flex>
-      </Flex>
+      </>
     );
   };
 
   return (
-    <FriendsListWrapper>
+    <>
       {/* TODO : isFavorite으로 즐찾해둔 친구 상단에 먼저 렌더링 */}
       {friendsData?.map((friend) => (
         <FriendCard key={friend.memberId}>
@@ -72,10 +123,11 @@ const FriendsList = () => {
             profileImgUrl={friend.profileImgUrl}
             name={friend.name}
             invitationLink={friend.invitationLink}
+            isFavorite={friend.isFavorite}
           />
         </FriendCard>
       ))}
-    </FriendsListWrapper>
+    </>
   );
 };
 
