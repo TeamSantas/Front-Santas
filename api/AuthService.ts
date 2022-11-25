45bda6462d02076bfa9b8c729c3cde0ab1d44ea8
shinequasar <shinequasar@gmail.com>
2022-11-25 16:15:55 +0900

    [feature/SAN-155]: [test]

--- api/AuthService.ts
+++ api/AuthService.ts
@@ -1,9 +1,9 @@
 import {AuthAuthInstance} from "./APIInstance";
 import {ResponseData} from "../util/type";
-//
-// class AuthService{
-//     //로그아웃 API 🔑
-//     //TODO : logoutData 명세해주기
-//     getLogout = () => AuthAuthInstance.get<ResponseData<any>>(`/auth/logout`);
-// }
-// export default new AuthService();
+
+class AuthService{
+    //로그아웃 API 🔑
+    //TODO : logoutData 명세해주기
+    getLogout = () => AuthAuthInstance.get<ResponseData<any>>(`/auth/logout`);
+}
+export default new AuthService();
--- api/FriendsService.ts
+++ api/FriendsService.ts
@@ -1,15 +1,15 @@
 import {FriendsAuthInstance} from "./APIInstance";
 import {FriendsData, MemberData, ResponseData} from "../util/type";
 
-// class FriendsService{
-//     //친구 목록 API 🔑
-//     getFriends = () => {
-//         return FriendsAuthInstance.get<ResponseData<FriendsData>>(`/api/friend`);
-//
-//     };
-//     //친구 리스트에 친구목록 저장 🔑 (카카오에서 받아와서 저장)
-//
-//     //링크로 친구 검색하기 🔑
-//
-// }
-// export default new FriendsService();
+class FriendsService{
+    //친구 목록 API 🔑
+    getFriends = () => {
+        return FriendsAuthInstance.get<ResponseData<FriendsData>>(`/api/friend`);
+
+    };
+    //친구 리스트에 친구목록 저장 🔑 (카카오에서 받아와서 저장)
+
+    //링크로 친구 검색하기 🔑
+
+}
+export default new FriendsService();
