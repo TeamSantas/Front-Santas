be74284ce41f44475f76632a9c90daa64af5f03f
shinequasar <shinequasar@gmail.com>
2022-11-26 18:11:35 +0900

    [feature/SAN-128]: [ADD] 캡쳐로직 수정 및 티켓이미지 추가

--- component/CustomModal.tsx
+++ component/CustomModal.tsx
@@ -1,7 +1,7 @@
 import styled, { css } from "styled-components";
 import RedButton from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
-import { CenteredFlex } from "../styles/styledComponentModule";
+import ShareAPIButton from "./share/ShareAPIButton";
 
 const CenteredModalWrapper = styled.div`
   margin: auto;
@@ -37,8 +37,36 @@ const CustomBody = styled(Modal.Body)`
 const CustomFooter = styled(Modal.Footer)`
   border: none !important;
 `;
-
+const TabButton = styled(RedButton)`
+  width: 250px;
+  margin: 20px;
+  padding: 7px;
+  color: white;
+  background-size: 5px 5px;
+  background: rgba(255,255,255,.2);
+  text-shadow: 1px 0 black;
+  font-weight: bold;
+  font-size: 24px;
+  border: none !important;
+  background: linear-gradient(45deg, #AC473D 25%, #C0544A 0, #C0544A 50%, #AC473D 0, #AC473D 75%, #C0544A 0 );
+  &:hover{
+    background: linear-gradient(45deg, #3C6C54 25%, #78AB91 0, #78AB91 50%, #3C6C54 0, #3C6C54 75%, #78AB91 0 );
+  }
+  &:active{
+    background: linear-gradient(45deg, #3C6C54 25%, #78AB91 0, #78AB91 50%, #3C6C54 0, #3C6C54 75%, #78AB91 0 );
+  }
+`
 const CustomModal = (props) => {
+  const onDownloadAs = (uri: string, filename: string) => {
+    console.log("다운됨");
+    const link = document.createElement("a");
+    document.body.appendChild(link);
+    link.href = uri;
+    link.download = filename;
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <Modal
       {...props}
@@ -59,24 +87,25 @@ const CustomModal = (props) => {
           {props.body && props.body}
           {props.text && <h2>{props.text}</h2>}
         </CustomBody>
-
         {/* ----------- footer ----------- */}
-        {props.buttons && (
-          <CenteredFlex>
-            <CustomFooter bgcolor={props.btncolor}>
-              {props.buttons.map((btn) => (
-                <CustomButtons
-                  key={btn.title}
-                  onClick={props.onHide}
-                  color={btn.color}
-                  bgcolor={btn.bgcolor}
-                >
-                  {btn.title}
-                </CustomButtons>
-              ))}
-            </CustomFooter>
-          </CenteredFlex>
-        )}
+        <ShareAPIButton/>
+        <TabButton onClick={()=>onDownloadAs(props.img, "pitapet_calendar.png")}>내 캘린더 저장하기</TabButton>
+            {/*{props.buttons && (*/}
+        {/*  <CenteredFlex>*/}
+        {/*    <CustomFooter bgcolor={props.btncolor}>*/}
+        {/*      {props.buttons.map((btn) => (*/}
+        {/*        <CustomButtons*/}
+        {/*          key={btn.title}*/}
+        {/*          onClick={props.onHide}*/}
+        {/*          color={btn.color}*/}
+        {/*          bgcolor={btn.bgcolor}*/}
+        {/*        >*/}
+        {/*          {btn.title}*/}
+        {/*        </CustomButtons>*/}
+        {/*      ))}*/}
+        {/*    </CustomFooter>*/}
+        {/*  </CenteredFlex>*/}
+        {/*)}*/}
       </CenteredModalWrapper>
     </Modal>
   );
