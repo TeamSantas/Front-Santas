5476e15dce858c33cda68cd6a3dc20f23526e746
shinequasar <shinequasar@gmail.com>
2022-11-26 18:05:07 +0900

    [feature/SAN-128]: [ADD] SEO 트위터용 추가

--- component/common/Seo.tsx
+++ component/common/Seo.tsx
@@ -8,15 +8,22 @@ export default function Seo({ title }) {
             {/*웹에 설정해줘야하는 og 메타태그*/}
             <link rel="icon" href="/assets/image/favicon.ico" />
             <meta property="og:type" content="website"/>
-            <meta property="og:url" content="httpㄴ://pitapat-adventcalendar.site/"/>
+            <meta property="og:url" content="https://pitapat-adventcalendar.site/"/>
             <meta property="og:title" content="다함께 크리스마스 즐기기✨"/>
             <meta property="og:image" content="https://pitapat-adventcalendar.site/assets/image/character/face_heart.png"/>
             <meta property="og:description" content="크리스마스다! 두근두근 어드벤트 캘린더"/>
             <meta property="og:site_name" content="두근두근 어드벤트 캘린더🎁"/>
             <meta property="og:locale" content="ko_KR"/>
-            {/*<다음의 태그는 필수는 아니지만, 포함하는 것을 추천함*/}
             <meta property="og:image:width" content="1200"/>
             <meta property="og:image:height" content="630"/>
+
+            {/*// 트위터 카드 타입*/}
+            <meta name="twitter:card" content="크리스마스다! 두근두근 어드벤트 캘린더"/>
+            <meta name="twitter:title" content="두근두근 어드벤트 캘린더🎁"/>
+            <meta name="twitter:description" content="다함께 크리스마스 즐기기✨"/>
+            <meta name="twitter:image" content="https://pitapat-adventcalendar.site/assets/image/character/face_heart.png"/>
+            <meta name="twitter:creator" content="@hayanco"/>
+            <meta name="twitter:site" content="https://pitapat-adventcalendar.site/"/>
         </Head>
     );
 }
