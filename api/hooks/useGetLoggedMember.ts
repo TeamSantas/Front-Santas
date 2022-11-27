7219f3f8030e04a07b015a2fb6d607da14177f7c
shinequasar <shinequasar@gmail.com>
2022-11-27 22:52:07 +0900

    [feature/SAN-128]: [refactor] 자잘한 수정

--- api/hooks/useGetLoggedMember.ts
+++
@@ -1,8 +0,0 @@
-// 실질적으로 api 당겨오는 로직을 처리하는 훅
-import MemberService from "../MemberService";
-
-export async function useGetLoggedMember() {
-    const res = await MemberService.getLoggedMember();
-    // console.log(res);
-    return res;
-}
