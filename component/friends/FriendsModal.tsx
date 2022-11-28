f447eca975e9ec1d9548bdccf54fe0c391b86a9f
Suyeon-B <tndus4243@naver.com>
2022-11-29 02:54:03 +0900

    fix : fixed path to rebuild

--- component/friends/FriendsModal.tsx
+++ component/friends/FriendsModal.tsx
@@ -1,26 +1,29 @@
 import { Modal } from "react-bootstrap";
-import { GreenCloseButton, CustomHeader, CustomBody, CustomFooter } from "../../styles/styledComponentModule";
+import {
+  CustomBody,
+  CustomFooter,
+  CustomHeader,
+  GreenCloseButton,
+} from "../../styles/styledComponentModule";
 import FriendsList from "./FriendsList";
 
 const FriendsModal = (props) => {
   return (
-      <Modal
-        {...props}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <CustomHeader>
-          <Modal.Title id="contained-modal-title-vcenter">
-            친구 목록
-          </Modal.Title>
-          <GreenCloseButton onClick={props.onHide} />
-        </CustomHeader>
-        <CustomBody>
-          <FriendsList />
-        </CustomBody>
-        <CustomFooter/>
-      </Modal>
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <CustomHeader>
+        <Modal.Title id="contained-modal-title-vcenter">친구 목록</Modal.Title>
+        <GreenCloseButton onClick={props.onHide} />
+      </CustomHeader>
+      <CustomBody>
+        <FriendsList />
+      </CustomBody>
+      <CustomFooter />
+    </Modal>
   );
 };
 
