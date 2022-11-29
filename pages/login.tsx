a950952772350d1027efa6c8157b8b09eb1f3f58
shinequasar <shinequasar@gmail.com>
2022-11-30 00:50:44 +0900

    [feature/SAN-128]: [test] 도메인에서 카카오연결테스트 성공

--- pages/login.tsx
+++ pages/login.tsx
@@ -38,7 +38,7 @@ const Login: NextPage = () => {
           <br /> 어드벤트 캘린더를 모으러 가볼까요?
         </h3>
         {/*<Link href={"https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=3c01bf310ee0268b13dab1daa6c3a78a&scope=account_email%20profile_nickname%20profile_image%20friends&state=ZG_0J4yTF5EXpiZdBZhoTUNkRyyeclSFvLjlJAe20_g%3D&redirect_uri=http://localhost:3000/oauth/callback/kakao"}>*/}
-        <Link href={"https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=3c01bf310ee0268b13dab1daa6c3a78a&scope=account_email%20profile_nickname%20profile_image%20friends&state=ZG_0J4yTF5EXpiZdBZhoTUNkRyyeclSFvLjlJAe20_g%3D&redirect_uri=https://pitapat-adventcalendar.site//oauth/callback/kakao"}>
+        <Link href={"https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=3c01bf310ee0268b13dab1daa6c3a78a&scope=account_email%20profile_nickname%20profile_image%20friends&state=ZG_0J4yTF5EXpiZdBZhoTUNkRyyeclSFvLjlJAe20_g%3D&redirect_uri=https://pitapat-adventcalendar.site/oauth/callback/kakao"}>
           <img src="/assets/image/kakao_login_large_narrow.png" width="222" />
         </Link>
       </Container>
