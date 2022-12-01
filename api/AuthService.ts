585270317ad6093217e021e0d26014119676364d
maltepoo <jaengpin@gmail.com>
2022-12-01 18:49:20 +0900

    [chore] jwt url prod -> dev

--- api/AuthService.ts
+++ api/AuthService.ts
@@ -5,7 +5,7 @@ import {useEffect, useState} from "react";
 class AuthService {
     //백엔드에 인가코드주고 Jwt 받아오기
     getKakaoLogin= (code, state) => {
-        const link = `http://ec2-43-201-99-216.ap-northeast-2.compute.amazonaws.com:8080/oauth/callback/kakao?code=${code}&state=${state}`
+        const link = `http://ec2-13-209-72-164.ap-northeast-2.compute.amazonaws.com:8080/oauth/callback/kakao?code=${code}&state=${state}`
         return AuthInstance.get<any>(link)
     }
 }
