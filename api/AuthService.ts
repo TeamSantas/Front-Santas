4a1aa71d32721fbcc388ef5da68f011c15b7ef03
shinequasar <shinequasar@gmail.com>
2022-12-03 23:46:14 +0900

    [feature/SAN-199]: [feat] baseUrl변경, https로 url 수정

--- api/AuthService.ts
+++ api/AuthService.ts
@@ -1,11 +1,9 @@
 import {  AuthInstance } from "./APIInstance";
-import {getCookie} from "../businesslogics/cookie";
-import {useEffect, useState} from "react";
 
 class AuthService {
     //백엔드에 인가코드주고 Jwt 받아오기
     getKakaoLogin= (code, state) => {
-        const link = `http://pitapat-adventcalendar.shop:8080/oauth/callback/kakao?code=${code}&state=${state}`
+        const link = `https://pitapat-adventcalendar.shop/oauth/callback/kakao?code=${code}&state=${state}`
         return AuthInstance.get<any>(link)
     }
 }
