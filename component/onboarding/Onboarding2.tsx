903e10f5b1fdb3e340e90c9122f5331849548177
maltepoo <jaengpin@gmail.com>
2022-12-06 22:17:24 +0900

    [chore] 온보딩 다음버튼 추가& css 약간수정

--- component/onboarding/Onboarding2.tsx
+++ component/onboarding/Onboarding2.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { GreenButton } from "../../styles/styledComponentModule";
 
 const OnboardingContainer = styled.div`
   display: ${({ pageNumber, showPage }) =>
@@ -10,18 +11,26 @@ const OnboardingContainer = styled.div`
 `;
 
 const Text = styled.div`
-  color: #ffffff;
   font-size: 24px;
+  color: white;
+  background-color: rgba(0, 0, 0, 0.2);
+  padding: 0 10px;
+  border-radius: 5px;
   text-align: center;
-  text-shadow:1px 1px 1px #000;
+  
   @media screen and (min-width: 1024px) {
     font-size: 36px;
+    color: #ffffff;
   }
 `;
 
 const Image = styled.img`
   width: 230px;
   margin-bottom: 60px;
+`;
+
+const NextBtn = styled(GreenButton)`
+  margin-top: 30px;
 `;
 
 export const Onboarding2 = ({ showPage }) => {
@@ -32,8 +41,11 @@ export const Onboarding2 = ({ showPage }) => {
       <Image src="/assets/image/onboarding/face_small_cry.png" />
       <Text>
         마을의 유일한 하얀코는,
-        <br /> 빨간코가 되고 싶어서 슬퍼했어요
       </Text>
+      <Text>
+        빨간코가 되고 싶어서 슬퍼했어요
+      </Text>
+      <NextBtn>다음으로</NextBtn>
     </OnboardingContainer>
   );
 };
