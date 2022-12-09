cb9343545bfbcb8628e7b3f3dbd0d0e80f047f9a
shinequasar <shinequasar@gmail.com>
2022-12-09 10:53:34 +0900

    [feature/friendButton]:[feat] 네이버 검색엔진 등록

--- component/common/Seo.tsx
+++ component/common/Seo.tsx
@@ -8,6 +8,8 @@ export default function Seo({ title }) {
         name="description"
         content="크리스마스다! 두근두근 어드벤트 캘린더"
       />
+        {/*네이버 검색엔진 등록*/}
+        <meta name="naver-site-verification" content="09946e86a7ef8d94f057b0f634f96410720f566c" />
       {/*웹에 설정해줘야하는 og 메타태그*/}
       <link rel="icon" href="/assets/image/favicon.ico" />
       <meta property="og:type" content="website" />
