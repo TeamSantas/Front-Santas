e99b71b71d1e084c1fa835728a5ff4f4cb2607b6
shinequasar <shinequasar@gmail.com>
2023-01-17 15:58:16 +0900

    [hotfix/url]:이스케이프_변환방지

--- pages/oauth/callback/kakao.tsx
+++ pages/oauth/callback/kakao.tsx
@@ -1,7 +1,7 @@
 import {NextPage} from "next";
 import {useContext, useEffect, useState} from "react";
 import {useRouter} from "next/router";
-import {KakaoLogin} from "../../../api/hooks/useKakaoLogin";
+import {KakaoLogin, KakaoTmpLogin} from "../../../api/hooks/useKakaoLogin";
 import {storeContext} from "../../../store/Store";
 import SettingService from "../../../api/SettingService";
 import {setCookie} from "../../../businesslogics/cookie";
@@ -16,9 +16,17 @@ const Kakao : NextPage = () => {
     //로그인하는 함수(인가코드 받아와 백엔드에서 두 Token을 받아옴)
     const run = async () => {
         try {
-            const res = await KakaoLogin(new URL(window.location.href).searchParams.get("code"), new URL(window.location.href).searchParams.get("state"));
+            let CODE = String.raw`${new URL(window.location.href).searchParams.get("code")}`;
+            let STATUS = String.raw`${new URL(window.location.href).searchParams.get("state")}`;
+
+            if(CODE.slice(-1,CODE.length)===`=`) CODE=CODE.replace(/.$/,'%3D')
+            if(STATUS.slice(-1,CODE.length)===`=`) STATUS=STATUS.replace(/.$/,'%3D')
+            // const res = await KakaoLogin(new URL(window.location.href).searchParams.get("code"), new URL(window.location.href).searchParams.get("state"));
+
+            const res = await KakaoLogin(CODE, STATUS);
             await setCookie("token", res.data.data.token, 30);
             await setCookie('subToken', res.data.data.refreshToken,30);
+
             // setRefresh(res.data.data.refreshToken);
             // setIsLogin(true);
 
