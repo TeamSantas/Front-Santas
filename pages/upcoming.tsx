586cc018bbefb3f5e0b968f2c4f6d71a8c9bc3f5
Suyeon-B <tndus4243@naver.com>
2023-11-09 14:10:57 +0900

    chore: image file name 수정

--- pages/upcoming.tsx
+++ pages/upcoming.tsx
@@ -117,7 +117,7 @@ const Promotion = () => {
         {[1, 2, 3, 4, 5].map((idx) => (
           <SwiperSlide key={idx}>
             <Image
-              src={`/assets/image/upcoming/feature${idx}.png`}
+              src={`/assets/image/upcoming/feature-${idx}.png`}
               alt={`feature${idx}`}
               fill
             />
