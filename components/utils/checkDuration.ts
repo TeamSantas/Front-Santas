58ecfc71eecdfb5c588a3f2aa12a0b63fbd4ddfd
Suyeon-B <tndus4243@naver.com>
2023-11-12 00:54:35 +0900

    feat: checkDuration util 함수

---
+++ components/utils/checkDuration.ts
@@ -0,0 +1,10 @@
+import moment from "moment";
+
+interface ICheckDuration {
+  startDate?: string; // ex) "2023-09-27T10:00:00.000+09:00"
+  endDate?: string; //     ㄴ> 23년 9월 27일 오전 10시(한국 시간 기준)
+}
+
+export const checkDuration = ({ startDate, endDate }: ICheckDuration) => {
+  return moment().isBetween(startDate, endDate);
+};
