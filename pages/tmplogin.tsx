349b937bcc75658db7a5ccb81a4552049cc447ab
shinequasar <shinequasar@gmail.com>
2023-11-12 03:37:55 +0900

    feat: 로그인버튼 도메인 변경

--- pages/tmplogin.tsx
+++ pages/tmplogin.tsx
@@ -9,9 +9,9 @@ import AuthService from "../api/AuthService";
 const Tmplogin = () => {
     return (
         <div>
-            <button onClick={()=>AuthService.getFirstKakaoLogin('https://pitapat-adventcalendar.shop/oauth2/authorization/kakao')}>
+            <button onClick={()=>AuthService.getFirstKakaoLogin('https://merry-christmas.site/oauth2/authorization/kakao')}>
                 새로운 카카오 로그인버튼</button>
-            <Link href={"https://pitapat-adventcalendar.shop/oauth2/authorization/kakao"}>
+            <Link href={"https://merry-christmas.site/oauth2/authorization/kakao"}>
                 <img src="/assets/image/kakao_login_large_narrow.png" width="222" />
             </Link>
         </div>
