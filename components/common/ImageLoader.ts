7d257b83466539eb5bf98b2dea7098068477cad3
Suyeon-B <tndus4243@naver.com>
2023-11-19 01:35:59 +0900

    fix: image loader - production에서만 사용

--- components/common/ImageLoader.ts
+++ components/common/ImageLoader.ts
@@ -1,6 +1,8 @@
-import {ImageLoaderProps} from "next/image";
+import { ImageLoaderProps } from "next/image";
 
-const imageLoader = ({src, width, quality}: ImageLoaderProps) => {
-    return `${process.env.NEXT_PUBLIC_DOMAIN}/${src}?w=${width}&q=${quality || 75}`;
+const imageLoader = ({ src, width, quality }: ImageLoaderProps) => {
+  return `${process.env.NEXT_PUBLIC_DOMAIN}/${src}?w=${width}&q=${
+    quality || 75
+  }`;
 };
 export default imageLoader;
--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const env = process.env.APP_ENV || "local";
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
@@ -11,10 +13,14 @@ const nextConfig = {
       },
     removeConsole: process.env.NODE_ENV === "production",
   },
-  images: {
-    loader: "custom",
-    loaderFile: "./components/common/ImageLoader.ts",
-  },
+  ...(process.env.NODE_ENV === "production"
+    ? {
+        images: {
+          loader: "custom",
+          loaderFile: "./components/common/ImageLoader.ts",
+        },
+      }
+    : {}),
 };
 
 //React Three Fiber 의존성
