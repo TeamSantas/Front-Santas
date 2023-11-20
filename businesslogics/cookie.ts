fccba0316c252518f0abe08e2afc4b3662b30bb1
Suyeon-B <tndus4243@naver.com>
2023-11-21 00:57:21 +0900

    feat: new-login

--- businesslogics/cookie.ts
+++ businesslogics/cookie.ts
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { parse, serialize } from "cookie";
 
 export const getCookie = (name: string) => {
   const cookieString = `; ${document.cookie}`;
@@ -8,22 +9,19 @@ export const getCookie = (name: string) => {
   } else return "";
 };
 
-export const setCookie = (name, value, days = null) => {
-  let expires = "";
-  const domain = document.location.origin.includes(".spartacodingclub.kr")
-    ? ".spartacodingclub.kr"
-    : document.location.hostname;
-  const sameSite = document.location.origin.includes(".spartacodingclub.kr")
-    ? "; SameSite=None; Secure"
-    : "";
-  let date = new Date();
-  date.setTime(
-    days ? date.getTime() + days * 24 * 60 * 60 * 1000 : date.getTime()
-  );
-  expires = `; domain=${domain}; expires=${date.toUTCString()}${sameSite}`;
-  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+/**
+ * 반드시 getServerSideProps에서 사용해주세요.
+ * context는 getServerSideProps(context)로 받아 넘겨주세요.
+ */
+export const setCookie = (name, value, context) => {
+  const cookie = serialize(name, value, {
+    maxAge: 60 * 60 * 24 * 7 * 5, // 5 week (초 단위)
+  });
+  context.res.setHeader("Set-Cookie", cookie);
 };
 
-export const removeCookie = (name : string) => {
-  document.cookie = name +`=; path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT; domain=${document.location.hostname};`;
+export const removeCookie = (name: string) => {
+  document.cookie =
+    name +
+    `=; path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT; domain=${document.location.hostname};`;
 };
