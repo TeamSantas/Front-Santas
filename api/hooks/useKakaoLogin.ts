8897f46de24d94542c393f95d086342e5859483e
Suyeon-B <tndus4243@naver.com>
2023-11-21 00:57:49 +0900

    delete: 불필요한 resouces 제거

--- api/hooks/useKakaoLogin.ts
+++ api/hooks/useKakaoLogin.ts
@@ -1,22 +1,7 @@
-import AuthService from "../AuthService";
 import { removeCookie } from "../../businesslogics/cookie";
-import { AuthAuthInstance } from "../APIInstance";
-import { ResponseData } from "../../util/type";
 
 export const kakaoLogout = () => {
   removeCookie("token");
   // AuthAuthInstance.get<ResponseData<any>>(`/api/member/logout`);
   return "logout_ok";
 };
-
-//엑세스토큰 받아오는 url
-export async function kakaoLogin() {
-  try {
-    const res = await AuthService.getKakaoLogin();
-    const oAuthLoginUrl = res?.data?.data;
-
-    console.log("oAuthLoginUrl: ", oAuthLoginUrl);
-  } catch (e) {
-    return e;
-  }
-}
