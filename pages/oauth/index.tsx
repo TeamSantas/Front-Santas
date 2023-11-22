2493fe388db51b8d512c5a3e06f7499b9d120d18
Suyeon-B <tndus4243@naver.com>
2023-11-22 11:28:22 +0900

    fix: query masking

--- pages/oauth/index.tsx
+++ pages/oauth/index.tsx
@@ -7,15 +7,10 @@ const OAuthLogin = () => {
 export async function getServerSideProps(context) {
   const { token } = context.query;
   setCookie("token", token, context);
-  const redirectUrl = "/";
-
-  // context 객체의 url 속성을 변경하여 리다이렉트
-  context.res.setHeader("Location", redirectUrl);
-  context.res.statusCode = 302;
-  context.res.end();
-
+  context.query = {}; // 쿼리 삭제
   return {
     props: {},
+    redirect: { destination: "/" },
   };
 }
 
