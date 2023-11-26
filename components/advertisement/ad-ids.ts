dd167dc6eff476697b64afd69dae20a12d23de31
Suyeon-B <tndus4243@naver.com>
2023-11-26 20:55:57 +0900

    add: 광고 id 모음 파일

---
+++ components/advertisement/ad-ids.ts
@@ -0,0 +1,43 @@
+export const friendsModalAdID = "DAN-6BIJLeLkllPU4bcX";
+export const upcomingAdID = "DAN-0tM6LqB1xeL7qG7S";
+export const informationModalAdID = "DAN-Q8XUVcYuooaQGG8h";
+
+// 두두타 광고들
+export const getTownAdID = (idx: number = 0) => {
+  const townAdIDs = [
+    townAdID0,
+    townAdID1,
+    townAdID2,
+    townAdID3,
+    townAdID4,
+    townAdID5,
+    townAdID6,
+    townAdID7,
+    townAdID8,
+    townAdID9,
+    townAdID10,
+    townAdID11,
+    townAdID12,
+    townAdID13,
+    townAdID14,
+    townAdID15,
+  ];
+
+  return townAdIDs[idx];
+};
+export const townAdID0 = "DAN-wnWIF3POirxtXIdG";
+export const townAdID1 = "DAN-4hgJdFJUcKqjNbTQ";
+export const townAdID2 = "DAN-qPP2J1Ldb9RJS5cB";
+export const townAdID3 = "DAN-Bo5nk2RZeoIzBKqY";
+export const townAdID4 = "DAN-6rj4bceEFZw1m9L2";
+export const townAdID5 = "DAN-Zo4jQYTZXzDWJgBu";
+export const townAdID6 = "DAN-Exfukt0vXOdjf0wc";
+export const townAdID7 = "DAN-yorpEkVDQxxs1W9B";
+export const townAdID8 = "DAN-HzgGoxlhESEDp2rc";
+export const townAdID9 = "DAN-aePkNT3it3Wpcvcg";
+export const townAdID10 = "DAN-vxaZz94rhTF9l2sc";
+export const townAdID11 = "DAN-42BzMLyjPeHHgebF";
+export const townAdID12 = "DAN-DmGywkjUeS8sAEV9";
+export const townAdID13 = "DAN-eeIboW3JuRJi1IVD";
+export const townAdID14 = "DAN-RbYzYzZKUWZmkHyB";
+export const townAdID15 = "DAN-mlBL0CoLfbDNmqYw";
