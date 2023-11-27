356c76b721c6f202415783e8e90d85f17891488d
Suyeon-B <tndus4243@naver.com>
2023-11-28 04:05:20 +0900

    feat: 받은 / 보낸 편지 리스트

--- components/tab/SendPresentList.tsx
+++ components/tab/SendPresentList.tsx
@@ -1,34 +1,12 @@
-import TabCard from "./TabCard";
-import { Flex } from "../../styles/styledComponentModule";
-import styled from "styled-components";
 import Card from "../receivedPresents/Card";
 import { useEffect, useState } from "react";
 import PresentService from "../../api/PresentService";
-
-const TabFlex = styled(Flex)`
-  flex-direction: row;
-  flex-wrap: wrap;
-  text-align: center;
-`;
-
-const PrepareingContainer = styled.div`
-  margin: 0 auto;
-`
-const PrepareingHeader = styled.h3`
-  display: block;
-  background-color: rgba(0, 0, 0, 0.1);
-  border-radius: 5px;
-  padding: 2px 10px;
-`;
-const Img = styled.img`
-  width: 150px;
-  margin-top: 40px;
-  margin-bottom: 10px;
-`;
+import { TabFlex, PreparingContainer, Img } from "./ReceivedPresentList";
 
 const SendPresentList = () => {
   const [sentPresentList, setSentPresentList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const presentLength = sentPresentList.length;
 
   useEffect(() => {
     const initSendPresentList = async () => {
@@ -40,34 +18,41 @@ const SendPresentList = () => {
   }, []);
 
   return (
-    <TabFlex>
-      {isLoading ?
-        <PrepareingContainer>
-          <Img src="/assets/image/character/spinner.gif" alt="로딩하얀코"/>
-          <PrepareingHeader>선물 불러오는중...</PrepareingHeader>
-        </PrepareingContainer> :
+    <TabFlex hasPresent={!presentLength || isLoading ? "true" : "false"}>
+      {isLoading ? (
+        <PreparingContainer>
+          <Img src="/assets/image/character/spinner.gif" alt="로딩하얀코" />
+          <PreparingContainer>선물 불러오는중...</PreparingContainer>
+        </PreparingContainer>
+      ) : (
         <>
-          {sentPresentList.length > 0 ? sentPresentList.map((present) => (
-            <Card
-              key={present.id}
-              id={present.id}
-              to={present.receiverName}
-              from={present.nickname}
-              date={present.receivedDate}
-              contents={present.contents}
-              thumbnail={present.imageURL}
-              type={"SEND"}
-              isRead={true}
-            />
-          )):
-            (<PrepareingContainer>
-              <Img src="/assets/image/character/face_cry.png" alt="울고있는하얀코"/>
-              <PrepareingHeader>보낸선물이 없습니다</PrepareingHeader>
-            </PrepareingContainer>
-            )
-          }
+          {sentPresentList.length > 0 ? (
+            sentPresentList.map((present) => (
+              <Card
+                key={present.id}
+                id={present.id}
+                to={present.receiverName}
+                from={present.nickname}
+                date={present.receivedDate}
+                contents={present.contents}
+                thumbnail={present.imageURL}
+                type={"SEND"}
+                isRead={true}
+              />
+            ))
+          ) : (
+            <PreparingContainer>
+              <Img
+                src="/assets/image/character/face_cry.png"
+                alt="울고있는하얀코"
+              />
+              <PreparingContainer>
+                앗! 아직 보낸 선물이 없어요.
+              </PreparingContainer>
+            </PreparingContainer>
+          )}
         </>
-      }
+      )}
     </TabFlex>
   );
 };
