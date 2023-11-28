10ca3f8afbb26b33f1feace79fe8c082b269a40f
Suyeon-B <tndus4243@naver.com>
2023-11-29 03:59:21 +0900

    fix: 선물 모달 css 수정

--- components/receivedPresents/PresentModal.tsx
+++ components/receivedPresents/PresentModal.tsx
@@ -73,4 +73,11 @@ const BlueBackground = styled.div`
 
 const CustomBody = styled(Modal.Body)`
   border: none;
+  height: 40vh;
+  overflow-y: scroll;
+  -ms-overflow-style: none; /* Explorer */
+  scrollbar-width: none; /* Firefox */
+  &::-webkit-scrollbar {
+    display: none; /* Chrome */
+  }
 `;
