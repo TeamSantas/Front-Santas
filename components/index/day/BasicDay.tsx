173dcbcafbf52367215fdece12be84dcc394bc4d
shinequasar <shinequasar@gmail.com>
2023-11-30 21:36:21 +0900

    feat: 오늘 날짜 기준으로 캘린더 불켜주기 & 이미지 최적화

--- components/index/day/BasicDay.tsx
+++ components/index/day/BasicDay.tsx
@@ -9,20 +9,32 @@ interface BasicDayProps {
 
 const BasicDay = (props:BasicDayProps) => {
     const {day,handleShow} = props;
-    return (
+    let color : string = '#1C3249';
+    let open = 'off'
+
+    const date = new Date()
+    const today = date.getDate();
+    const month = date.getMonth();
+    if(day<=today && month == 12 || month == 1){
+        color = '#FBCF6E';
+        open = 'on'
+      }
+
+  return (
         <td>
             <NumberOfReceivedPresents day={day}/>
-            <DayImg src={`/asset_ver2/image/days/day${day}.svg`}
+            <DayImg src={`/asset_ver2/image/day/${open}/${open}_day${day}.png`}
                     width={100}
                     height={100}
                     layout="responsive"
+                    color={color}
                     onClick={() => handleShow(day)}
                     alt={`day${day}`}/>
         </td>
     );
 };
 export default BasicDay;
-const DayImg = styled(Image)` 
+const DayImg = styled(Image)<{ color?: string }>` 
   justify-content: center;
   padding: 2px;
   align-items: center;
@@ -30,4 +42,6 @@ const DayImg = styled(Image)`
   height: auto;
   cursor: pointer;
   z-index: 10;
+  background-color:  ${(props) => props.color || '#1C3249'};
+  border-radius: 15px;
 `;
--- components/index/day/LongDay.tsx
+++ components/index/day/LongDay.tsx
@@ -7,20 +7,32 @@ interface LongDayProps {
 }
 
 const LongDay = (props:LongDayProps) => {
-    const {day,handleShow} = props;
-    return (
+  const {day,handleShow} = props;
+  let color : string = '#1C3249';
+  let open = 'off'
+
+  const date = new Date()
+  const today = date.getDate();
+  const month = date.getMonth();
+  if(day<=today && month == 12 || month == 1){
+      color = '#FBCF6E';
+      open = 'on'
+    }
+
+  return (
         <td rowSpan={2}>
             <NumberOfReceivedPresents day={day}/>
-            <DayImg src={`/asset_ver2/image/days/day${day}.svg`}
+            <DayImg src={`/asset_ver2/image/day/${open}/${open}_day${day}.png`}
                     width={`100`}
                     height={`100`}
+                    color={color}
                     onClick={() => handleShow(day)}
                     alt={`day${day}`}/>
         </td>
     );
 };
 export default LongDay;
-const DayImg = styled(Image)` 
+const DayImg = styled(Image)<{ color?: string }>` 
   justify-content: center;
   padding: 2px;
   align-items: center;
@@ -28,4 +40,6 @@ const DayImg = styled(Image)`
   height: 100%;
   cursor: pointer;
   z-index: 10;
+  background-color:  ${(props) => props.color || '#1C3249'};
+  border-radius: 15px;
 `;
