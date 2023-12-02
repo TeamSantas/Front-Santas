9071cef58c0a53bc91182ab458922aad8d01de28
shinequasar <shinequasar@gmail.com>
2023-12-03 05:50:10 +0900

    feat: 도움말 컨텐츠 swiper 수정

--- components/index/InformationModal.tsx
+++ components/index/InformationModal.tsx
@@ -1,28 +1,28 @@
-import { Modal } from "react-bootstrap";
-import {
-  GreenCloseButton,
-  CustomHeader,
-  CustomFooter,
-  CustomDescriptionBody,
-} from "../../styles/styledComponentModule";
+import {Modal} from "react-bootstrap";
+import {CustomDescriptionBody, CustomFooter, CustomHeader, GreenCloseButton,} from "../../styles/styledComponentModule";
 import AdFitModal from "../advertisement/adFitModal";
-import { informationModalAdID } from "../advertisement/ad-ids";
+import {informationModalAdID} from "../advertisement/ad-ids";
+import {Swiper, SwiperSlide} from "swiper/react";
+import {EffectFade, Navigation, Pagination} from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/effect-fade";
+import React, {useState} from "react";
+import styled from "styled-components";
+import {header_1, header_2, header_3, slide_1, slide_2, slide_3} from "./informationModaltext";
 
 const InformationModal = (props) => {
+  const [index, setIndex] = useState<number>(0);
   // info modal
-  const header = "어드벤트 캘린더란 ❓";
-  const text = `12월 1일부터 25일까지, 크리스마스를 기다리면서
-하나씩 선물을 열어보는 달력이에요. 📆 
-
-어드벤트 캘린더를 온라인으로 옮겨봤어요.
-
-미래의 캘린더 조각은 ❗️절대❗️ 열어볼 수 없답니다!
-오늘은 무슨 선물을 받았을까, 하는 💗두근두근한 마음💗을 드려요.
-
--------------
-🔗 버튼으로 내 링크를 복사해 친구에게 보내보세요! 
-      친구들이 내 캘린더에 찾아와 선물을 줄거예요.
--------------`;
+  const headers = [header_1, header_2, header_3];
+  const infoSlides = [slide_1,slide_2,slide_3]
+  const handleSlideChange = (swiper) => {
+    // 슬라이드가 변경될 때마다 호출되는 함수
+    const currentIndex = swiper.activeIndex;
+    setIndex((currentIndex));
+    console.log("현재 슬라이드 인덱스:", currentIndex);
+  };
   return (
     <AdFitModal
       {...props}
@@ -31,14 +31,60 @@ const InformationModal = (props) => {
       centered
       adFitId={informationModalAdID}
     >
-      <CustomHeader>
-        <Modal.Title id="contained-modal-title-vcenter">{header}</Modal.Title>
+      <Header>
+        <Modal.Title id="contained-modal-title-vcenter">{headers[index]}</Modal.Title>
         <GreenCloseButton onClick={props.onHide} />
-      </CustomHeader>
-      <CustomDescriptionBody>{text}</CustomDescriptionBody>
+      </Header>
+      <CustomDescriptionBody>
+        <StyledSwiper
+          className="mySwiper"
+          modules={[EffectFade, Navigation, Pagination]}
+          navigation
+          pagination={{
+            type: 'fraction',
+          }}
+          effect={"fade"}
+          slidesPerView={1}
+          onClickNext={(swiper) => swiper.next()}
+          onSlideChange={(swiper) => handleSlideChange(swiper)}
+        >
+          {infoSlides.map((text, idx) => (
+            <SwiperSlide key={idx}>
+              <Text>
+                {text}
+              </Text>
+            </SwiperSlide>
+          ))}
+        </StyledSwiper>
+      </CustomDescriptionBody>
       <CustomFooter />
     </AdFitModal>
   );
 };
 
 export default InformationModal;
+const Header = styled(CustomHeader)`
+  margin-top: 25px;
+  text-align: center;
+`;
+
+const StyledSwiper = styled(Swiper)`
+  max-width: 450px;
+  width: 88vw;
+  padding: 0;
+  .swiper-button-prev,
+  .swiper-button-next {
+    color: #605b5b;
+    z-index: 10;
+    width: 5px;
+    //버튼 보이고싶으면 풀기
+    display: none;
+  }
+`;
+
+const Text = styled.div`
+  width: 100%;
+  background-color: #fff;
+  padding: 30px 50px;
+  margin: 0 auto;
+`;
