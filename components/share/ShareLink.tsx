fee2c7ba421bc6e88ca42c6c5c3533f81c351b2e
Suyeon-B <tndus4243@naver.com>
2023-12-04 01:16:11 +0900

    chore: handle modals globaly

--- components/share/ShareLink.tsx
+++ components/share/ShareLink.tsx
@@ -1,44 +1,34 @@
 import Image from "next/image";
-import { useState } from "react";
 import styled from "styled-components";
-import CopyModal from "../index/CopyModal";
-import { loginUserDataAtom } from "../../store/globalState";
+import { loginUserDataAtom, modalStateAtom } from "../../store/globalState";
 import { useAtom } from "jotai";
 
 export const ShareLink = () => {
-  const [copyModal, setCopyModal] = useState<boolean>(false);
   const [storeUserData] = useAtom(loginUserDataAtom);
-
-  const clickCopyIconHandler = () => setCopyModal(true);
-  const handleCopyModalClose = () => setCopyModal(false);
+  const [, setShowModal] = useAtom(modalStateAtom);
 
   const linkCopyHandler = async () => {
     const copyURL = `https://merry-christmas.site/${storeUserData.invitationLink}`;
     try {
       await navigator.clipboard.writeText(copyURL);
-      setCopyModal(true);
+      setShowModal({
+        label: "copy",
+        show: true,
+      });
     } catch (e) {
       alert(
         "내 초대링크를 복사해 보내보세요! 바로 복사를 원하신다면~? 크롬브라우저로 접속해보세요✨"
       );
-      clickCopyIconHandler();
     }
   };
   return (
-    <>
-      <ShareBtn
-        src={`/asset_ver2/image/btn/link_btn.png`}
-        width={44}
-        height={44}
-        alt={"링크복사버튼"}
-        onClick={linkCopyHandler}
-      />
-      <CopyModal
-        link={`https://merry-christmas.site/${storeUserData.invitationLink}`}
-        show={copyModal}
-        onHide={handleCopyModalClose}
-      />
-    </>
+    <ShareBtn
+      src={`/asset_ver2/image/btn/link_btn.png`}
+      width={44}
+      height={44}
+      alt={"링크복사버튼"}
+      onClick={linkCopyHandler}
+    />
   );
 };
 
