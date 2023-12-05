e88362fcc1cac11884808d541d9a576b6a632d49
Suyeon-B <tndus4243@naver.com>
2023-12-05 17:52:50 +0900

    fix: token expire issue

--- api/hooks/useMember.ts
+++ api/hooks/useMember.ts
@@ -1,29 +1,11 @@
-import { defaultMemberData, defaultMemberRawData } from "../../util/type";
 import MemberService from "../MemberService";
-
-export async function getLoggedMember() {
-  try {
-    const res = await MemberService.getLoggedMember();
-    if (res.status === 200) {
-      return res.data.data.member;
-    }
-    return defaultMemberData;
-  } catch (e) {
-    console.error(e);
-    return defaultMemberData;
-  }
-}
 
 export async function getLoggedMemberRaw() {
   try {
     const res = await MemberService.getLoggedMember();
-    if (res.status === 200) {
-      return res.data.data;
-    }
-    return defaultMemberRawData;
+    return res;
   } catch (e) {
     console.error(e);
-    return defaultMemberRawData;
   }
 }
 
