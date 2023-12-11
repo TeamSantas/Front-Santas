fbb7a08cb8c3c94d9264ff64a5d778db826dabfc
Suyeon-B <tndus4243@naver.com>
2023-12-11 11:18:34 +0900

    fix: base url 수정

--- components/common/urls.ts
+++ components/common/urls.ts
@@ -1,1 +1,1 @@
-export const kakaoLoginUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/oauth2/authorization/kakao`;
+export const kakaoLoginUrl = `${process.env.NEXT_PUBLIC_BASE_URL}oauth2/authorization/kakao`;
