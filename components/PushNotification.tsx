72ce611a3bdbe1a092cf1b1d475858ad814a48da
Suyeon-B <tndus4243@naver.com>
2023-12-15 02:16:58 +0900

    feat: web push

---
+++ components/PushNotification.tsx
@@ -0,0 +1,64 @@
+import { useEffect } from "react";
+import SettingService from "../api/SettingService";
+import { onMessage, getMessaging, getToken } from "firebase/messaging";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASEURL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORABEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
+
+const PushNotification = () => {
+  const onMessageFCM = async () => {
+    try {
+      const permission = await Notification.requestPermission();
+      if (permission !== "granted") return;
+
+      // Initialize Firebase Cloud Messaging and get a reference to the service
+      const firebaseApp = initializeApp(firebaseConfig);
+      const messaging = getMessaging(firebaseApp);
+
+      getToken(messaging, {
+        vapidKey: process.env.NEXT_PUBLIC_FIREBASE_VAPID,
+      })
+        .then((currentToken) => {
+          if (currentToken) {
+            SettingService.setFcmtoken(currentToken);
+          } else {
+            console.log(
+              "No registration token available. Request permission to generate one."
+            );
+          }
+        })
+        .catch((err) => {
+          console.log("An error occurred while retrieving token. ", err);
+        });
+
+      onMessage(messaging, (payload) => {
+        const title = payload.notification.title;
+        const body = payload.notification.body;
+        const icon =
+          "https://merry-christmas.site/asset_ver2/image/common/title-logo.png";
+        const link = "https://merry-christmas.site/";
+        const options = { body, icon, link };
+
+        new Notification(title, options);
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    onMessageFCM();
+  }, []);
+
+  return <></>;
+};
+export default PushNotification;
--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,27 +1,32 @@
-importScripts('https://www.gstatic.com/firebasejs/9.14.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.14.0/firebase-messaging-compat.js');
+importScripts(
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js"
+);
+importScripts(
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js"
+);
 
-const config = {
+const firebaseConfig = {
   apiKey: "AIzaSyCastD4wzjzV_ABpqjpaPiX1t2d2pkWDiM",
   authDomain: "ddac-e6757.firebaseapp.com",
-  databaseURL: "https://ddac-e6757-default-rtdb.asia-southeast1.firebasedatabase.app",
+  databaseURL:
+    "https://ddac-e6757-default-rtdb.asia-southeast1.firebasedatabase.app/",
   projectId: "ddac-e6757",
   storageBucket: "ddac-e6757.appspot.com",
   messagingSenderId: "50350895067",
   appId: "1:50350895067:web:559369e927aac45508c1fe",
-  measurementId: "G-478S8PF211"
-}
+  measurementId: "G-478S8PF211",
+};
 
-firebase.initializeApp(config);
-const messaging = firebase.messaging();
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging(firebaseApp);
 
 messaging.onBackgroundMessage((payload) => {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  // 추후 background notice 커스텀시 사용
-  // const notificationTitle = 'Background Message Title';
-  // const notificationOptions = {
-  //   body: 'Background Message body.',
-  //   icon: '/firebase-logo.png'
-  // };
-  // self.registration.showNotification(notificationTitle, notificationOptions);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    link: payload.notification.link,
+    body: payload.notification.body,
+    icon: payload.notification.icon,
+  };
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
